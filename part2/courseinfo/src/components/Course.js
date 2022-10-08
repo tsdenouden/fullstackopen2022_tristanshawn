d49073bea43160ba2d7470560ad75fe2cb4b4c71
Tristan Shawn den Ouden <trisdo687@gmail.com>
2022-10-08 16:01:02 +0200

    2.1-2.5

--- part2/courseinfo/src/components/Course.js
+++ part2/courseinfo/src/components/Course.js
@@ -1,14 +1,24 @@
 const Header = ({text}) => <h1>{text}</h1>
 
-const Part = ({content}) => <li>{content}</li>
+const Part = ({content}) => (
+    <li>{content.name} {content.exercises}</li>
+)
 
 const Content = ({parts}) => {
+    const total = parts.reduce((sum, part) => {
+        return sum + part.exercises
+    }, 0)
     return (
-        <ul>
-            {parts.map(part =>
-                <Part key={part.id} content={part.name}/>
-            )}
-        </ul>
+        <div>
+            <ul style={{listStylePosition: "none", padding: 0}}>
+                {parts.map(part =>
+                    <Part key={part.id} content={part}/>
+                )}
+            </ul>
+            <p>
+                <b>total of {total} exercises</b>
+            </p>
+        </div>
     )
 }
 
