6b73f907bfab32757a006f90d26d44a2330ab89d
tsdenouden <trisdo687@gmail.com>
2022-10-23 16:08:38 +0200

    ex3.19-3.21

--- part3/phonebookBackend/index.js
+++ part3/phonebookBackend/index.js
@@ -41,40 +41,29 @@ app.get('/api/persons/:id', (request, response, next) => {
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :jsondata'))
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
     const body = request.body
-
-    if (body.name === undefined || body.number === undefined) {
-        return response.status(400).json({
-            error: 'content missing'
-        })
-    }
 
     const person = new Person({
         name: body.name,
         number: body.number
     })
 
-    person.save().then(savedPerson => {
+    person.save()
+        .then(savedPerson => {
         response.json(savedPerson)
     })
+        .catch(err => next(err))
 })
 
 app.put('/api/persons/:id', (request, response, next) => {
-    const body = request.body
+    const { name, number } = request.body
 
-    if (!body.name || !body.number) {
-        return response.status(400).json({
-            error: 'content missing'
-        })
-    }
-
-    const person = {
-        name: body.name,
-        number: body.number
-    }
-
-    Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    Person.findByIdAndUpdate(
+        request.params.id, 
+        { name, number },
+        { new: true, runValidators: true, context: 'query' },
+    )
         .then(updatedPerson => {
             response.json(updatedPerson)
         })
@@ -101,7 +90,9 @@ const errorHandler = (error, request, response, next) => {
 
     if (error.name === 'CastError') {
         return response.status(400).send({ error: 'malformatted id' })
-    } 
+    } else if (error.name === 'ValidationError') {
+        return response.status(400).json({ error: error.message })
+    }
 
     next(error)
 }
--- part3/phonebookBackend/models/person.js
+++ part3/phonebookBackend/models/person.js
@@ -13,10 +13,35 @@ mongoose.connect(url)
     })
 
 const personSchema = new mongoose.Schema({
-    name: String,
-    number: Number
+    name: {
+        type: String,
+        minLength: 3,
+        required: true
+    },
+    number: {
+        type: String,
+        required: true,
+        validate: [(val) => {
+            // check if the number split into more than 2 parts
+            if ((val.split('-').length) >= 3) {
+                return false
+            }
+            // check if the first part has less than 2 numbers
+            if ((val.indexOf('-')) === 1) {
+                return false
+            }
+            // check if there are letters in the number
+            if (!Number(val.replace('-',''))) {
+                return false
+            }
+            // check if the number is longer than 8 characters
+            const number = val.replace('-','')
+            return number.length >= 8
+        }, 'Please enter a valid number.']
+    }
 })
 
+// change id from object to string & delete _id, __v
 personSchema.set('toJSON', {
     transform: (document, returnedObject) => {
         returnedObject.id = String(returnedObject._id)
