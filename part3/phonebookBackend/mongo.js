44ce6440bc6e961090a595d9a0a03f33510b1b85
Tristan Shawn den Ouden <trisdo687@gmail.com>
2023-07-03 11:55:19 +0200

    part4

--- part3/phonebookBackend/mongo.js
+++ part3/phonebookBackend/mongo.js
@@ -7,7 +7,7 @@ if (process.argv.length < 3) {
 
 const password = process.argv[2]
 
-const url = `mongodb+srv://fullstack:${password}@cluster0.cgs2e1p.mongodb.net/?retryWrites=true&w=majority`
+const url = `mongodb+srv://fullstackuser:${password}@cluster0.cgs2e1p.mongodb.net/?retryWrites=true&w=majority`
 
 const personSchema = new mongoose.Schema({
   name: String,
